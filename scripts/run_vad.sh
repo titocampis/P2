d488b310a5c7e6236f8807eabbaf3307b847fa95
Albino Nogueiras Rodríguez <albino.nogueiras@upc.edu>
2020-02-24 22:34:27 +0100

    Ficheros Iniciales de la Práctica

---
+++ scripts/run_vad.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Be sure that this file has execution permissions:
+# Use the nautilus explorer or chmod +x run_vad.sh
+
+
+DB=/home/albino/PAV/Prácticas/enunciados/obsoletos/P2/db.test
+CMD=bin/vad  #write here the name and path of your program
+
+for filewav in $DB/*/*wav; do
+#    echo
+    echo "**************** $filewav ****************"
+    if [[ ! -f $filewav ]]; then 
+	    echo "Wav file not found: $filewav" >&2
+	    exit 1
+    fi
+
+    filevad=${filewav/.wav/.vad}
+
+    $CMD -i $filewav -o $filevad || exit 1
+
+# Alternatively, uncomment to create output wave files
+#    filewavOut=${filewav/.wav/.vad.wav}
+#    $CMD $filewav $filevad $filewavOut || exit 1
+
+done
+
+vad_evaluation.pl $DB/*/*lab
+
+exit 0
